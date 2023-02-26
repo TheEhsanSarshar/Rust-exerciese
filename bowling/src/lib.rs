5db7a8c0728b8a8c299c2cd0a346b505e2ea588f
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-02-26 17:12:24 +0430

    create dot-dsl exercise

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,18 +4,52 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+#[derive(Clone, Copy)]
+pub struct Frame {
+    score: u16,
+    reamained_pins: u16,
+    number_of_balls: u16,
+}
+pub struct BowlingGame {
+    frames: [Frame; 10],
+    /* current frame */
+    cursor: u16,
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        let mut to_return = BowlingGame {
+            cursor: 0,
+            frames: [Frame {
+                score: 0,
+                reamained_pins: 10,
+                number_of_balls: 2,
+            }; 10],
+        };
+        to_return.frames[9].number_of_balls = 3;
+        to_return.frames[9].reamained_pins = 30;
+        to_return
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        let mut current_frame = &mut self.frames[self.cursor as usize];
+        if current_frame.reamained_pins < pins {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+        current_frame.reamained_pins -= pins;
+        if current_frame.reamained_pins == 0 {
+            self.cursor += 1
+        }
+        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        Some(0);
+        if self.cursor == 9 {
+            Some(0)
+        } else {
+            None
+        }
+        // unimplemented!("Return the score if the game is complete, or None if not.");
     }
 }
